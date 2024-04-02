05dfb832dd8c0edb973d3751610053dda87f61d9
Szymon Choruży <szychoruzy@gmail.com>
2024-04-02 12:25:51 +0200

    pierwsze zadania

--- public/index.php
+++ public/index.php
@@ -1,3 +1,46 @@
 <?php
 
-echo 'Test' . (5 + 5);
+//Silnia
+//Napisz funkcję obliczającą silnię 
+//liczby podanej jako parametr. W przypadku błędnego parametru funkcja powinna zwrócić -1
+
+function silnia ($x) {
+	
+	if (!is_string($x)) {
+		
+		$result = 1;
+			
+		for($j = 1; $j <=$x; $j++) {
+			$result *= $j;
+		}
+		
+		return $result;
+
+	} else {
+		return -1;
+	}
+}
+
+echo silnia(4);
+echo "<br>";
+echo "-----------------";
+echo "<br>";
+
+for($x = 1; $x <=50; $x++){
+    if($x % 4 ==0 && $x % 5 == 0 ){
+
+        echo "YingYang ";
+
+    } elseif($x % 5 == 0){
+
+        echo "Yang ";
+
+    } elseif($x % 4 == 0){
+
+        echo "Ying ";
+
+    } else{
+
+        echo $x. " ";
+    }
+}
