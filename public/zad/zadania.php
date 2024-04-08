f73b1d157dfc072b3ae9dccecbb2690d0ee0234a
Szymon Choruży <szychoruzy@gmail.com>
2024-04-08 10:31:13 +0200

    kolejne zadania wykonane

--- public/zad/zadania.php
+++ public/zad/zadania.php
@@ -23,4 +23,30 @@ $dt3 = strtotime("+".$days." days", strtotime($dt));
 
 echo "<br>". date('Y-m-d', $dt3);
 
-//
+// Write a PHP function to get start and end date of a week (by week number) of a particular year.
+
+function Start_End_Date_of_a_week($week , $year)
+{
+    $time = strtotime("1 January $year", time());
+    $day = date('w', $time);
+    $time += ((7*$week)+1-$day)*24*3600;
+    $dates[0] = date('Y-n-j', $time);
+    $time += 6*24*3600;
+    $dates[1] = date('Y-n-j',$time);
+    return $dates; 
+}
+
+$result = Start_End_Date_of_a_week(12,2024);
+
+echo "<br>";
+echo $result[0]."\n";
+echo $result[1];
+
+// Write a PHP script to calculate the current age of a person. 
+$bday = new DateTime('26.5.2006');
+$today = new Datetime(date('m.d.y'));
+
+$diff = $today->diff($bday);
+
+printf(' Your age : %d years, %d month, %d days', $diff->y, $diff->m, $diff->d);
+printf("\n"); 
