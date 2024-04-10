ebd370e9d8a36ceb89346d63969f83f9b97708b1
Szymon Choruży <szychoruzy@gmail.com>
2024-04-10 09:14:36 +0200

    Nowy projekt dodanie formularza

---
+++ public/projekt/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company</title>
+</head>
+<body>
+
+    <form action="" method="post">
+        <h3>Rejestracja</h3>
+        <input type="text" name="uid" placeholder="Username"><br><br>
+        <input type="password" name="pwd" placeholder="Password"><br><br>
+        <input type="password" name="pwdrepeat" placeholder="Repeat Password"><br><br>
+        <button type="submit" name="submit">SIGN UP</button>
+    </form>
+
+    <br><br>
+
+    <form action="" method="post">
+        <h3>Logowanie:</h3>
+        <input type="text" name="uid" placeholder="Username"><br><br>
+        <input type="password" name="pwd" placeholder="Password"><br><br>
+        <button type="submit" name="sumbit">LOGIN</button>
+    </form>
+
+</body>
+</html>
