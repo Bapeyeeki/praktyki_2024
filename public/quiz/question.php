5da895f6088269053dc8eaaa6f6a51358d06d92e
Szymon Choruży <szychoruzy@gmail.com>
2024-04-17 13:58:54 +0200

    dalsze poprawki

--- public/quiz/question.php
+++ public/quiz/question.php
@@ -16,16 +16,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['answers'])) {
     // Inicjalizacja licznika poprawnych odpowiedzi
     $score = 0;
 
+    // Tablica przechowująca poprawne odpowiedzi
+    $correctAnswers = array();
+
     // Porównanie udzielonych odpowiedzi z prawidłowymi odpowiedziami
     foreach ($selectedAnswers as $index => $selectedAnswer) {
         $correctAnswer = $questions[$index]['correct_answer'];
         if ($selectedAnswer === $correctAnswer) {
             $score++;
         }
+        // Dodanie poprawnej odpowiedzi do tablicy
+        $correctAnswers[$index] = $correctAnswer;
     }
 
     // Wyświetlenie wyniku
     echo "<h1>Twój wynik: $score/$totalQuestions</h1>";
+
+    // Wyświetlenie poprawnych odpowiedzi
+    echo "<h2>Poprawne odpowiedzi:</h2>";
+    foreach ($questions as $index => $question) {
+        $selectedAnswer = isset($selectedAnswers[$index]) ? $selectedAnswers[$index] : 'Brak odpowiedzi';
+        $correctAnswer = $correctAnswers[$index];
+        
+        echo "<p>Pytanie: " . htmlspecialchars($question['question']) . "</p>";
+        echo "<p>Twoja odpowiedź: " . htmlspecialchars($selectedAnswer) . "</p>";
+        echo "<p>Poprawna odpowiedź: " . htmlspecialchars($correctAnswer) . "</p>";
+
+        // Sprawdzenie poprawności odpowiedzi i kolorowanie
+        if ($selectedAnswer === $correctAnswer) {
+            echo "<p style='color: green;'>Odpowiedź poprawna!</p>";
+        } else {
+            echo "<p style='color: red;'>Odpowiedź niepoprawna.</p>";
+        }
+        echo "<hr>";
+    }
 
     // Usunięcie sesji z pytaniami po sprawdzeniu wyniku
     unset($_SESSION['questions']);
